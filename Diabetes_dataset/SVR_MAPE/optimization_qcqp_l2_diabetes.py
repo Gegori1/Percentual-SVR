5c9bf180a08ceed2817f9d4f9edfe14e945889ed
Gegori1 <galvarez.alvarez@iteso.mx>
2025-10-22 23:13:03 -0600

    optimization for the svr mape diabetes

--- Diabetes_dataset/SVR_MAPE/optimization_qcqp_l2_diabetes.py
+++ Diabetes_dataset/SVR_MAPE/optimization_qcqp_l2_diabetes.py
@@ -1,60 +1,55 @@
 # %%
 import optuna
 import pandas as pd
-from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_percentage_error as mape
 from sklearn.model_selection import train_test_split
 
-from svr_qcqp_multi_kernel_l2 import svr_qcqp_multi_kernel_l2
-from optimizer_crossvalidation import CrossValidationOptimizer
-import argparse
-
-# %% arguments
+from gcp_library.optimization_utils.optimizer_crossvalidation import CrossValidationOptimizer
+from percentual_svr_package.svr_ls_rmspe import svr_ls_rmspe
+# from percentual_svr_package.svr_mape import svr_original_mape
 
 
 
 # %% load data
 
-diabetes = pd.read_csv("diabetes.csv", delimiter="\t")
+diabetes = pd.read_csv("../data/diabetes.csv", delimiter="\t")
 X_train, X_test, y_train, y_test = train_test_split(diabetes.drop(columns="Y"), diabetes.Y, test_size=0.225, random_state=42)
 X_train, y_train = X_train.values, y_train.values
 # %% parameters
 
-logs_name = args.logs_file
+logs_name = "svr_qcqp"
 load_previous = False
 log_file = f"{logs_name}.jsonl"
 
 params = {
-    'C': (args.C_min, args.C_max, "float"),
-    'epsilon': (args.epsilon_min, args.epsilon_max, "float"),
-    'tau': (args.tau_min, args.tau_max, "float"),
+    'C': (1e-3, 1e6, "float"),
+    'gamma': (1e-3, 1e6, "float"),
+    'epsilon': (1e-3, 1e2, "float"),
     'kernel': ("rbf", 
         None, 
         "stationary"
     )
 }
 
-n_iter = args.n_iter
+n_iter = 200
 
 # %% Optimization
 
 optimizer = CrossValidationOptimizer(
     X_train, y_train, 
-    svr_qcqp_multi_kernel_l2, 
-    mean_absolute_percentage_error,
+    svr_ls_rmspe, 
+    mape,
     save_path=log_file,
-    cloud_name=args.cloud_name,
-    cloud_bucket=args.cloud_bucket,
-    cloud_key=args.cloud_key,
+    cloud_name="",
+    cloud_bucket="",
+    cloud_key="",
     param_distributions=params,
-    n_folds=args.n_splits,
+    n_folds=5,
     standardize=True,
-    upload_cloud_rate=args.cloud_uploadrate
+    n_jobs=-1,
+    upload_to_cloud=False
 )
 
 # Create a study object and optimize the objective function
-study = optuna.create_study(direction='minimize')
-study.optimize(optimizer, n_trials=n_iter, n_jobs=-1)
+study = optimizer.optimize(n_trials=n_iter)
 
-# Print the best hyperparameters and the best accuracy
-print("Best hyperparameters: ", study.best_params)
-print("Best accuracy: ", study.best_value)
